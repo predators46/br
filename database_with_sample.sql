ce534226ac06ced917091e51998ec81b3ece2f70
esaaprillia <esaapriliasalsabila@gmail.com>
2024-08-01 18:48:53 +0800

    Add files via upload

--- database_with_sample.sql
+++ database_with_sample.sql
@@ -73,7 +73,6 @@ CREATE TABLE `ci_sessions` (
 
 LOCK TABLES `ci_sessions` WRITE;
 /*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
-INSERT INTO `ci_sessions` VALUES ('90043b7d49337773ce2cd55a54fecfeb','0.0.0.0','Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en',1259772557,'a:9:{s:2:\"id\";s:1:\"1\";s:9:\"user_name\";s:5:\"admin\";s:10:\"country_id\";s:1:\"0\";s:5:\"email\";s:18:\"root@localhost.com\";s:4:\"role\";s:10:\"superadmin\";s:10:\"last_visit\";s:19:\"2009-11-30 19:29:20\";s:7:\"created\";s:19:\"2009-11-30 19:29:20\";s:8:\"modified\";s:19:\"0000-00-00 00:00:00\";s:7:\"perpage\";s:2:\"10\";}','');
 /*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -156,7 +155,6 @@ CREATE TABLE `fa_country` (
 
 LOCK TABLES `fa_country` WRITE;
 /*!40000 ALTER TABLE `fa_country` DISABLE KEYS */;
-INSERT INTO `fa_country` VALUES (1,'AF','Afghanistan','AFG',4),(2,'AL','Albania','ALB',8),(3,'DZ','Algeria','DZA',12),(4,'AS','American Samoa','ASM',16),(5,'AD','Andorra','AND',20),(6,'AO','Angola','AGO',24),(7,'AI','Anguilla','AIA',660),(8,'AQ','Antarctica',NULL,NULL),(9,'AG','Antigua and Barbuda','ATG',28),(10,'AR','Argentina','ARG',32),(11,'AM','Armenia','ARM',51),(12,'AW','Aruba','ABW',533),(13,'AU','Australia','AUS',36),(14,'AT','Austria','AUT',40),(15,'AZ','Azerbaijan','AZE',31),(16,'BS','Bahamas','BHS',44),(17,'BH','Bahrain','BHR',48),(18,'BD','Bangladesh','BGD',50),(19,'BB','Barbados','BRB',52),(20,'BY','Belarus','BLR',112),(21,'BE','Belgium','BEL',56),(22,'BZ','Belize','BLZ',84),(23,'BJ','Benin','BEN',204),(24,'BM','Bermuda','BMU',60),(25,'BT','Bhutan','BTN',64),(26,'BO','Bolivia','BOL',68),(27,'BA','Bosnia and Herzegovina','BIH',70),(28,'BW','Botswana','BWA',72),(29,'BV','Bouvet Island',NULL,NULL),(30,'BR','Brazil','BRA',76),(31,'IO','British Indian Ocean Territory',NULL,NULL),(32,'BN','Brunei Darussalam','BRN',96),(33,'BG','Bulgaria','BGR',100),(34,'BF','Burkina Faso','BFA',854),(35,'BI','Burundi','BDI',108),(36,'KH','Cambodia','KHM',116),(37,'CM','Cameroon','CMR',120),(38,'CA','Canada','CAN',124),(39,'CV','Cape Verde','CPV',132),(40,'KY','Cayman Islands','CYM',136),(41,'CF','Central African Republic','CAF',140),(42,'TD','Chad','TCD',148),(43,'CL','Chile','CHL',152),(44,'CN','China','CHN',156),(45,'CX','Christmas Island',NULL,NULL),(46,'CC','Cocos (Keeling) Islands',NULL,NULL),(47,'CO','Colombia','COL',170),(48,'KM','Comoros','COM',174),(49,'CG','Congo','COG',178),(50,'CD','Congo, the Democratic Republic of the','COD',180),(51,'CK','Cook Islands','COK',184),(52,'CR','Costa Rica','CRI',188),(53,'CI','Cote D\'Ivoire','CIV',384),(54,'HR','Croatia','HRV',191),(55,'CU','Cuba','CUB',192),(56,'CY','Cyprus','CYP',196),(57,'CZ','Czech Republic','CZE',203),(58,'DK','Denmark','DNK',208),(59,'DJ','Djibouti','DJI',262),(60,'DM','Dominica','DMA',212),(61,'DO','Dominican Republic','DOM',214),(62,'EC','Ecuador','ECU',218),(63,'EG','Egypt','EGY',818),(64,'SV','El Salvador','SLV',222),(65,'GQ','Equatorial Guinea','GNQ',226),(66,'ER','Eritrea','ERI',232),(67,'EE','Estonia','EST',233),(68,'ET','Ethiopia','ETH',231),(69,'FK','Falkland Islands (Malvinas)','FLK',238),(70,'FO','Faroe Islands','FRO',234),(71,'FJ','Fiji','FJI',242),(72,'FI','Finland','FIN',246),(73,'FR','France','FRA',250),(74,'GF','French Guiana','GUF',254),(75,'PF','French Polynesia','PYF',258),(76,'TF','French Southern Territories',NULL,NULL),(77,'GA','Gabon','GAB',266),(78,'GM','Gambia','GMB',270),(79,'GE','Georgia','GEO',268),(80,'DE','Germany','DEU',276),(81,'GH','Ghana','GHA',288),(82,'GI','Gibraltar','GIB',292),(83,'GR','Greece','GRC',300),(84,'GL','Greenland','GRL',304),(85,'GD','Grenada','GRD',308),(86,'GP','Guadeloupe','GLP',312),(87,'GU','Guam','GUM',316),(88,'GT','Guatemala','GTM',320),(89,'GN','Guinea','GIN',324),(90,'GW','Guinea-Bissau','GNB',624),(91,'GY','Guyana','GUY',328),(92,'HT','Haiti','HTI',332),(93,'HM','Heard Island and Mcdonald Islands',NULL,NULL),(94,'VA','Holy See (Vatican City State)','VAT',336),(95,'HN','Honduras','HND',340),(96,'HK','Hong Kong','HKG',344),(97,'HU','Hungary','HUN',348),(98,'IS','Iceland','ISL',352),(99,'IN','India','IND',356),(100,'ID','Indonesia','IDN',360),(101,'IR','Iran, Islamic Republic of','IRN',364),(102,'IQ','Iraq','IRQ',368),(103,'IE','Ireland','IRL',372),(104,'IL','Israel','ISR',376),(105,'IT','Italy','ITA',380),(106,'JM','Jamaica','JAM',388),(107,'JP','Japan','JPN',392),(108,'JO','Jordan','JOR',400),(109,'KZ','Kazakhstan','KAZ',398),(110,'KE','Kenya','KEN',404),(111,'KI','Kiribati','KIR',296),(112,'KP','Korea, Democratic People\'s Republic of','PRK',408),(113,'KR','Korea, Republic of','KOR',410),(114,'KW','Kuwait','KWT',414),(115,'KG','Kyrgyzstan','KGZ',417),(116,'LA','Lao People\'s Democratic Republic','LAO',418),(117,'LV','Latvia','LVA',428),(118,'LB','Lebanon','LBN',422),(119,'LS','Lesotho','LSO',426),(120,'LR','Liberia','LBR',430),(121,'LY','Libyan Arab Jamahiriya','LBY',434),(122,'LI','Liechtenstein','LIE',438),(123,'LT','Lithuania','LTU',440),(124,'LU','Luxembourg','LUX',442),(125,'MO','Macao','MAC',446),(126,'MK','Macedonia, the Former Yugoslav Republic of','MKD',807),(127,'MG','Madagascar','MDG',450),(128,'MW','Malawi','MWI',454),(129,'MY','Malaysia','MYS',458),(130,'MV','Maldives','MDV',462),(131,'ML','Mali','MLI',466),(132,'MT','Malta','MLT',470),(133,'MH','Marshall Islands','MHL',584),(134,'MQ','Martinique','MTQ',474),(135,'MR','Mauritania','MRT',478),(136,'MU','Mauritius','MUS',480),(137,'YT','Mayotte',NULL,NULL),(138,'MX','Mexico','MEX',484),(139,'FM','Micronesia, Federated States of','FSM',583),(140,'MD','Moldova, Republic of','MDA',498),(141,'MC','Monaco','MCO',492),(142,'MN','Mongolia','MNG',496),(143,'MS','Montserrat','MSR',500),(144,'MA','Morocco','MAR',504),(145,'MZ','Mozambique','MOZ',508),(146,'MM','Myanmar','MMR',104),(147,'NA','Namibia','NAM',516),(148,'NR','Nauru','NRU',520),(149,'NP','Nepal','NPL',524),(150,'NL','Netherlands','NLD',528),(151,'AN','Netherlands Antilles','ANT',530),(152,'NC','New Caledonia','NCL',540),(153,'NZ','New Zealand','NZL',554),(154,'NI','Nicaragua','NIC',558),(155,'NE','Niger','NER',562),(156,'NG','Nigeria','NGA',566),(157,'NU','Niue','NIU',570),(158,'NF','Norfolk Island','NFK',574),(159,'MP','Northern Mariana Islands','MNP',580),(160,'NO','Norway','NOR',578),(161,'OM','Oman','OMN',512),(162,'PK','Pakistan','PAK',586),(163,'PW','Palau','PLW',585),(164,'PS','Palestinian Territory, Occupied',NULL,NULL),(165,'PA','Panama','PAN',591),(166,'PG','Papua New Guinea','PNG',598),(167,'PY','Paraguay','PRY',600),(168,'PE','Peru','PER',604),(169,'PH','Philippines','PHL',608),(170,'PN','Pitcairn','PCN',612),(171,'PL','Poland','POL',616),(172,'PT','Portugal','PRT',620),(173,'PR','Puerto Rico','PRI',630),(174,'QA','Qatar','QAT',634),(175,'RE','Reunion','REU',638),(176,'RO','Romania','ROM',642),(177,'RU','Russian Federation','RUS',643),(178,'RW','Rwanda','RWA',646),(179,'SH','Saint Helena','SHN',654),(180,'KN','Saint Kitts and Nevis','KNA',659),(181,'LC','Saint Lucia','LCA',662),(182,'PM','Saint Pierre and Miquelon','SPM',666),(183,'VC','Saint Vincent and the Grenadines','VCT',670),(184,'WS','Samoa','WSM',882),(185,'SM','San Marino','SMR',674),(186,'ST','Sao Tome and Principe','STP',678),(187,'SA','Saudi Arabia','SAU',682),(188,'SN','Senegal','SEN',686),(189,'CS','Serbia and Montenegro',NULL,NULL),(190,'SC','Seychelles','SYC',690),(191,'SL','Sierra Leone','SLE',694),(192,'SG','Singapore','SGP',702),(193,'SK','Slovakia','SVK',703),(194,'SI','Slovenia','SVN',705),(195,'SB','Solomon Islands','SLB',90),(196,'SO','Somalia','SOM',706),(197,'ZA','South Africa','ZAF',710),(198,'GS','South Georgia and the South Sandwich Islands',NULL,NULL),(199,'ES','Spain','ESP',724),(200,'LK','Sri Lanka','LKA',144),(201,'SD','Sudan','SDN',736),(202,'SR','Suriname','SUR',740),(203,'SJ','Svalbard and Jan Mayen','SJM',744),(204,'SZ','Swaziland','SWZ',748),(205,'SE','Sweden','SWE',752),(206,'CH','Switzerland','CHE',756),(207,'SY','Syrian Arab Republic','SYR',760),(208,'TW','Taiwan, Province of China','TWN',158),(209,'TJ','Tajikistan','TJK',762),(210,'TZ','Tanzania, United Republic of','TZA',834),(211,'TH','Thailand','THA',764),(212,'TL','Timor-Leste',NULL,NULL),(213,'TG','Togo','TGO',768),(214,'TK','Tokelau','TKL',772),(215,'TO','Tonga','TON',776),(216,'TT','Trinidad and Tobago','TTO',780),(217,'TN','Tunisia','TUN',788),(218,'TR','Turkey','TUR',792),(219,'TM','Turkmenistan','TKM',795),(220,'TC','Turks and Caicos Islands','TCA',796),(221,'TV','Tuvalu','TUV',798),(222,'UG','Uganda','UGA',800),(223,'UA','Ukraine','UKR',804),(224,'AE','United Arab Emirates','ARE',784),(225,'GB','United Kingdom','GBR',826),(226,'US','United States','USA',840),(227,'UM','United States Minor Outlying Islands',NULL,NULL),(228,'UY','Uruguay','URY',858),(229,'UZ','Uzbekistan','UZB',860),(230,'VU','Vanuatu','VUT',548),(231,'VE','Venezuela','VEN',862),(232,'VN','Viet Nam','VNM',704),(233,'VG','Virgin Islands, British','VGB',92),(234,'VI','Virgin Islands, U.s.','VIR',850),(235,'WF','Wallis and Futuna','WLF',876),(236,'EH','Western Sahara','ESH',732),(237,'YE','Yemen','YEM',887),(238,'ZM','Zambia','ZMB',894),(239,'ZW','Zimbabwe','ZWE',716);
 /*!40000 ALTER TABLE `fa_country` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -190,7 +188,6 @@ CREATE TABLE `fa_user` (
 
 LOCK TABLES `fa_user` WRITE;
 /*!40000 ALTER TABLE `fa_user` DISABLE KEYS */;
-INSERT INTO `fa_user` VALUES (1,'admin',0,'8709d7dc8f2ba1f9bd2b7140ff7078c2','root@localhost.com','superadmin',0,NULL,'2009-12-02 20:48:11','2009-12-02 13:48:11','0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `fa_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -216,7 +213,6 @@ CREATE TABLE `fa_user_profile` (
 
 LOCK TABLES `fa_user_profile` WRITE;
 /*!40000 ALTER TABLE `fa_user_profile` DISABLE KEYS */;
-INSERT INTO `fa_user_profile` VALUES (1,'Admin','Mr. Admin','102');
 /*!40000 ALTER TABLE `fa_user_profile` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -388,7 +384,6 @@ CREATE TABLE `postplan` (
 
 LOCK TABLES `postplan` WRITE;
 /*!40000 ALTER TABLE `postplan` DISABLE KEYS */;
-INSERT INTO `postplan` VALUES (1,'packet',NULL),(2,'time',1),(3,'bw_download',64000),(4,'bw_upload',32000),(5,'idletimeout',0);
 /*!40000 ALTER TABLE `postplan` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -410,7 +405,9 @@ CREATE TABLE `radacct` (
   `nasportid` varchar(15) DEFAULT NULL,
   `nasporttype` varchar(32) DEFAULT NULL,
   `acctstarttime` datetime DEFAULT NULL,
+  `acctupdatetime` datetime DEFAULT NULL,
   `acctstoptime` datetime DEFAULT NULL,
+  `acctinterval` int(12) DEFAULT NULL,
   `acctsessiontime` int(12) DEFAULT NULL,
   `acctauthentic` varchar(32) DEFAULT NULL,
   `connectinfo_start` varchar(50) DEFAULT NULL,
@@ -423,15 +420,24 @@ CREATE TABLE `radacct` (
   `servicetype` varchar(32) DEFAULT NULL,
   `framedprotocol` varchar(32) DEFAULT NULL,
   `framedipaddress` varchar(15) NOT NULL DEFAULT '',
+  `framedipv6address` varchar(45) NOT NULL DEFAULT '',
+  `framedipv6prefix` varchar(45) NOT NULL DEFAULT '',
+  `framedinterfaceid` varchar(44) NOT NULL DEFAULT '',
+  `delegatedipv6prefix` varchar(45) NOT NULL DEFAULT '',
   `acctstartdelay` int(12) DEFAULT NULL,
   `acctstopdelay` int(12) DEFAULT NULL,
   `xascendsessionsvrkey` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`radacctid`),
   KEY `username` (`username`),
   KEY `framedipaddress` (`framedipaddress`),
+  KEY `framedipv6address` (`framedipv6address`),
+  KEY `framedipv6prefix` (`framedipv6prefix`),
+  KEY `framedinterfaceid` (`framedinterfaceid`),
+  KEY `delegatedipv6prefix` (`delegatedipv6prefix`),
   KEY `acctsessionid` (`acctsessionid`),
   KEY `acctuniqueid` (`acctuniqueid`),
   KEY `acctstarttime` (`acctstarttime`),
+  KEY `acctinterval` (`acctinterval`),
   KEY `acctstoptime` (`acctstoptime`),
   KEY `nasipaddress` (`nasipaddress`)
 ) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
@@ -443,7 +449,6 @@ CREATE TABLE `radacct` (
 
 LOCK TABLES `radacct` WRITE;
 /*!40000 ALTER TABLE `radacct` DISABLE KEYS */;
-INSERT INTO `radacct` VALUES (8,'4ad2b83900000000','d4cb615e5aca3202','08-00-27-02-31-64','','','0.0.0.0','0','Wireless-802.11','2009-10-12 12:01:45','2009-10-12 12:02:30',45,'','','',34725,293327,'08-00-27-D2-C9-84','08-00-27-02-31-64','Lost-Carrier','','','192.168.182.3',0,0,''),(9,'4ad2b8b500000000','da22abb0ce17a5fa','08-00-27-02-31-64','','','0.0.0.0','0','Wireless-802.11','2009-10-12 12:03:49','2009-10-12 12:25:36',1307,'','','',101684,201401,'08-00-27-D2-C9-84','08-00-27-02-31-64','Idle-Timeout','','','192.168.182.6',0,0,''),(10,'4adbdcb000000000','4500e517a52b8912','08-00-27-02-31-64','','','0.0.0.0','0','Wireless-802.11','2009-10-19 10:27:44','2009-10-19 10:52:22',1478,'','','',46050,342791,'08-00-27-D2-C9-84','08-00-27-02-31-64','Idle-Timeout','','','192.168.182.3',0,0,'');
 /*!40000 ALTER TABLE `radacct` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -575,7 +580,6 @@ CREATE TABLE `radpostauth` (
 
 LOCK TABLES `radpostauth` WRITE;
 /*!40000 ALTER TABLE `radpostauth` DISABLE KEYS */;
-INSERT INTO `radpostauth` VALUES (1,'08-00-27-60-3F-CA','password','Access-Accept','2009-10-11 15:26:54'),(2,'cubvap14','pinsabip','Access-Accept','2009-10-11 15:41:04'),(3,'pandu','pandu','Access-Accept','2009-10-11 15:43:08'),(4,'pandu','a','Access-Accept','2009-10-11 15:43:31'),(5,'moyvor13','pandu','Access-Accept','2009-10-11 15:53:30'),(6,'pandu','a','Access-Accept','2009-10-12 02:15:54'),(7,'pandu','a','Access-Accept','2009-10-12 02:25:38'),(8,'08-00-27-02-31-64','password','Access-Accept','2009-10-12 05:01:45'),(9,'08-00-27-02-31-64','password','Access-Accept','2009-10-12 05:03:49'),(10,'08-00-27-02-31-64','password','Access-Accept','2009-10-19 03:27:44'),(11,'08-00-27-02-31-64','password','Access-Accept','2009-10-19 16:57:16'),(12,'golpaz11','a','Access-Accept','2009-10-19 16:58:36'),(13,'golpaz11','a','Access-Accept','2009-10-19 16:59:19'),(14,'dulrel15','a','Access-Accept','2009-10-28 11:59:17'),(15,'a','a','Access-Accept','2009-10-28 12:02:07'),(16,'a','a','Access-Accept','2009-10-28 12:11:06'),(17,'a','a','Access-Accept','2009-10-28 12:14:32'),(18,'a','a','Access-Accept','2009-10-28 12:38:15'),(19,'a','a','Access-Accept','2009-10-28 12:40:45'),(20,'a','a','Access-Accept','2009-10-28 12:43:47'),(21,'a','a','Access-Accept','2009-10-28 12:46:04'),(22,'a','a','Access-Accept','2009-10-28 12:51:04'),(23,'a','a','Access-Accept','2009-10-28 12:53:13'),(24,'a','a','Access-Accept','2009-10-28 12:54:44'),(25,'a','a','Access-Accept','2009-10-28 12:55:42'),(26,'a','a','Access-Accept','2009-10-28 12:58:57'),(27,'a','a','Access-Accept','2009-10-28 13:01:53'),(28,'a','a','Access-Accept','2009-10-28 13:11:45'),(29,'a','a','Access-Accept','2009-10-28 13:13:05'),(30,'a','a','Access-Accept','2009-10-28 13:14:24'),(31,'a','a','Access-Accept','2009-10-28 13:41:21'),(32,'a','a','Access-Accept','2009-10-28 13:46:26'),(33,'08-00-27-02-31-64','password','Access-Accept','2009-10-28 13:51:31');
 /*!40000 ALTER TABLE `radpostauth` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -603,7 +607,6 @@ CREATE TABLE `radreply` (
 
 LOCK TABLES `radreply` WRITE;
 /*!40000 ALTER TABLE `radreply` DISABLE KEYS */;
-INSERT INTO `radreply` VALUES (18,'08-00-27-02-31-61','WISPr-Session-Terminate-Timev',':=','2009-October-30T24:00:00'),(19,'08-00-27-02-31-6a','WISPr-Session-Terminate-Time',':=','2009-November-9T24:00:00'),(153,'xekgix6','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(154,'tamvop6','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(155,'sehyuh8','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(156,'rikpux10','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(157,'yokney6','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(158,'xuryec7','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(159,'sitsal10','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(160,'sambov11','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(161,'paymog7','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(162,'tubyap15','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(164,'00-00-00-00-01','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(165,'civsiz15','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(166,'xowzud11','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(167,'peknim10','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(168,'tagxon7','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(169,'gewdam9','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(170,'soksiz14','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(171,'rihsop11','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(172,'pirbis5','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(173,'kulzob13','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(174,'wemben6','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(175,'mubcaw15','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(176,'wisciy5','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(177,'zagpos7','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(178,'runyoc11','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(179,'yoxdek9','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(180,'pecnas10','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(181,'yucbel15','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(182,'hinmuz14','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(183,'kaczus5','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(184,'hukgux6','WISPr-Session-Terminate-Time',':=','2009-12-3T24:00:00'),(185,'100','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(186,'10','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(187,'120','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(188,'123','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(189,'13','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(190,'123123','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(191,'43423423','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(192,'123131','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(193,'1231313','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(194,'44241','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(195,'23231','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(196,'123131323131','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(197,'1312311123','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00'),(198,'q123123','WISPr-Session-Terminate-Time',':=','2009-12-12T24:00:00');
 /*!40000 ALTER TABLE `radreply` ENABLE KEYS */;
 UNLOCK TABLES;
 
